2a1012a0915bbc9462a616c5402bc3d03e84c3f2
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-06-24 10:42:02 +0530

    Updates

---
+++ precache-manifest.fc243485d796e1ec330ecb001f21bf98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c11e66199c4dd8a115d6d88f1fd56b",
+    "url": "/Regstration-Form-reactJs/index.html"
+  },
+  {
+    "revision": "58cf84b597112a651c87",
+    "url": "/Regstration-Form-reactJs/static/css/main.dde56b6d.chunk.css"
+  },
+  {
+    "revision": "5c367cadbc116f39dcc2",
+    "url": "/Regstration-Form-reactJs/static/js/2.c4d3b3aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Regstration-Form-reactJs/static/js/2.c4d3b3aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cf84b597112a651c87",
+    "url": "/Regstration-Form-reactJs/static/js/main.f680894b.chunk.js"
+  },
+  {
+    "revision": "107c076246e25b65de07",
+    "url": "/Regstration-Form-reactJs/static/js/runtime-main.ff0e2540.js"
+  }
+]);
